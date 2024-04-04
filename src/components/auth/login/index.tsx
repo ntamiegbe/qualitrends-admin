88ff9884a2a7e4bf3d253316b36cbd1090ea5a88
Richard Ingwe <richardingwe5@gmail.com>
2024-04-04 08:43:10 +0100

    fix: mobile view for login.

--- src/components/auth/login/index.tsx
+++ src/components/auth/login/index.tsx
@@ -21,17 +21,21 @@ const Login = () => {
 	} = methods;
 
 	return (
-		<div className='w-full bg-white min-h-screen grid grid-cols-3'>
-			<main className='col-span-1 flex items-center justify-center'>
-				<div className='space-y-10 w-full max-w-[332px] px-4'>
-					<div className='space-y-2 text-center'>
-						<h1 className='text-3xl font-semibold'>Welcome Back!</h1>
-						<p>Log in with your details below</p>
+		<div className='w-full bg-white min-h-screen grid grid-cols-1 lg:grid-cols-3 gap-y-5'>
+			<main className='col-span-1 order-2 lg:order-1 flex items-center justify-center'>
+				<div className='space-y-6 lg:space-y-10 w-full max-lg:container lg:max-w-[332px] lg:px-4'>
+					<div className='space-y-1 lg:space-y-2 text-center'>
+						<h1 className='text-2xl lg:text-3xl font-semibold'>
+							Welcome Back!
+						</h1>
+						<p className='text-sm lg:text-base'>
+							Log in with your details below
+						</p>
 					</div>
 
 					<FormProvider {...methods}>
 						<form>
-							<div className='space-y-6'>
+							<div className='space-y-4 lg:space-y-6'>
 								<Input
 									label='Email Address'
 									name='email'
@@ -56,6 +60,7 @@ const Login = () => {
 							<div className='mt-6 flex items-center space-x-4 justify-between'>
 								<Checkbox
 									name='rememberMe'
+									id='rememberMe'
 									label='Keep me logged in'
 									labelClassName='text-sm'
 								/>
@@ -63,7 +68,7 @@ const Login = () => {
 									Forgot Password?
 								</button>
 							</div>
-							<div className='max-w-[240px] mt-20 mx-auto'>
+							<div className='max-w-[240px] my-11 lg:mt-20 mx-auto'>
 								<Button disabled={!isValid} className='w-full'>
 									Login
 								</Button>
@@ -72,12 +77,12 @@ const Login = () => {
 					</FormProvider>
 				</div>
 			</main>
-			<aside className='col-span-2 py-10 px-16'>
-				<div className='relative h-full'>
+			<aside className='col-span-2 lg:order-2 py-1.5 lg:py-10 max-lg:container lg:px-16'>
+				<div className='relative h-[366px] lg:h-full'>
 					<div className='absolute w-full h-full border-[5px] border-primary'></div>
 					<div className='relative w-full h-full'>
-						<div className="h-[calc(100%-128px)] w-full flex justify-center items-center absolute  top-1/2 transform -translate-y-1/2 -left-16 bg-[url('/assets/images/auth-bg.jpg')] bg-cover bg-no-repeat bg-center">
-							<Icons.Logo />
+						<div className="h-[calc(100%-40px)] lg:h-[calc(100%-128px)] w-full flex justify-center items-center absolute top-1/2 transform -translate-y-1/2 -left-5 lg:-left-16 bg-[url('/assets/images/auth-bg.jpg')] bg-cover bg-no-repeat bg-center">
+							<Icons.Logo className='w-[99px] h-[74px] lg:w-[300px] lg:h-[225px]' />
 						</div>
 					</div>
 				</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -37,6 +37,16 @@ const config: Config = {
 				Roboto: ["var(--roboto)"],
 				OpenSans: ["var(--open-sans)"],
 			},
+			container: {
+				center: true,
+				padding: {
+					DEFAULT: "1rem",
+					sm: "2rem",
+					lg: "4rem",
+					xl: "5rem",
+					"2xl": "6rem",
+				},
+			},
 		},
 	},
 	plugins: [],
