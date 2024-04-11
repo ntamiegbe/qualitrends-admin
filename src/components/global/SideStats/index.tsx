4420d4243e0d6c70b41e3a52821937443a619414
Richard Ingwe <richardingwe5@gmail.com>
2024-04-11 19:31:21 +0100

    Refactor: Add date-fns and echarts dependencies, and update Projects component

--- src/components/global/SideStats/index.tsx
+++ src/components/global/SideStats/index.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from "react";
+
 type SideStatsProps = {
 	stats: {
 		title: string;
@@ -10,7 +12,7 @@ const SideStats = ({ stats }: SideStatsProps) => {
 		<div className='px-2.5 py-3 flex items-center lg:py-6 bg-white drop-shadow-md rounded-lg h-full'>
 			<div className='w-full'>
 				{stats.map((stat, index) => (
-					<>
+					<Fragment key={index}>
 						<div key={index} className='space-x-8 px-4'>
 							<span className='lg:text-xl font-bold'>{stat.value}</span>
 							<span className='max-lg:text-sm'>{stat.title}</span>
@@ -18,7 +20,7 @@ const SideStats = ({ stats }: SideStatsProps) => {
 						{index < stats.length - 1 && (
 							<hr className='border border-b border-[#CBCFD3] my-3' />
 						)}
-					</>
+					</Fragment>
 				))}
 			</div>
 		</div>
